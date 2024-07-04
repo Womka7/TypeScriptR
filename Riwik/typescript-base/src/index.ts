d6d2673ed1364e31b741b712f2d570fe7d1c2941
Karina P <karina_pineda97@hotmail.com>
2024-07-04 07:48:25 -0500

    organizate

--- Riwik/typescript-base/src/index.ts
+++ Riwik/typescript-base/src/index.ts
@@ -1,19 +1,22 @@
-// const form =document.querySelector(".new-item-form") as HTMLFormElement;
 
-// const type =document.querySelector("#type") as HTMLSelectElement;
-// const toFrom=document.querySelector("#tofrom") as HTMLInputElement;
-// const details =document.querySelector("#details") as HTMLInputElement;
-// const amount =document.querySelector("#amount")as HTMLInputElement;
+// ----dom con type script
 
-// form.addEventListener("submit",(ev:Event)=>{
-//   ev.preventDefault();
-//   console.log(`${type.value.toUpperCase()} is from: ${toFrom.value} details: ${details.value} the amount is: ${amount.valueAsNumber}`);
-// });
+const form =document.querySelector(".new-item-form") as HTMLFormElement;
+
+const type =document.querySelector("#type") as HTMLSelectElement;
+const toFrom=document.querySelector("#tofrom") as HTMLInputElement;
+const details =document.querySelector("#details") as HTMLInputElement;
+const amount =document.querySelector("#amount")as HTMLInputElement;
+
+form.addEventListener("submit",(ev:Event)=>{
+  ev.preventDefault();
+  console.log(`${type.value.toUpperCase()} is from: ${toFrom.value} details: ${details.value} the amount is: ${amount.valueAsNumber}`);
+});
 
 
 //-----------arreglos
 
-/*
+
 let  names :string[]=["mario","luigi", "yoshi"];
 names.push("princess");
 console.log(names);
@@ -22,10 +25,9 @@ console.log(names);
 let numbers:(number | string )[] =[1,2,3,4];
 numbers.push("30");
 console.log(numbers);
-*/
 
 //-----------objetos 
-/*
+
 let useer ={
   firstName:"steven",
   lastName:'Zuluaga',
@@ -39,10 +41,10 @@ useer={
 }
 
 console.log(useer.firstName.length);
-*/
+
 
 //-------------funciones
-/*
+
 const add=(num1:number,num2:number)=>{
   console.log(num1+num2);
 }
@@ -65,29 +67,3 @@ const add2=(num1:number,num2?:number):number=>{
   return suma
 }
 console.log(add2(5));
-*/
-
-//EJERICIO NUMERO 1
-//Programa que pide al usuario un numero y muestra si es par o impar
-
-const numero1=document.getElementById("numberOne") as HTMLInputElement;
-
-const form =document.querySelector(".new-item-form") as HTMLFormElement;
-
-form.addEventListener("submit", (event:Event)=>{
-  event.preventDefault();
-  console.log(numero1.value);
-  
-  typeNumber()
-});
-
-const typeNumber =()=>{
-  let dato =numero1.valueAsNumber;
- (dato%2)==0? alert("par"):alert("impar");
-}
-
-
-//EJERICIO NUMERO 2
-//Programa que pide al usuario que ingrese su nombre. Luego, verifica 
-// si el nombre ingresado no es nulo ni está vacío. Si el nombre es válido, 
-// muestra un mensaje de bienvenida; de lo contrario, muestra un mensaje de error.
