43cfb5af50b06cc7a8a7077522e76a2a75616a4d
Karina P <karina_pineda97@hotmail.com>
2024-07-05 08:07:38 -0500

    fix: update of date api

--- apiPlatzi/dist/index.js
+++ apiPlatzi/dist/index.js
@@ -12,11 +12,25 @@ function llamadoApi() {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield fetch("https://api.escuelajs.co/api/v1/products");
         const productsApi = yield response.json();
-        productsApi.forEach(product, {
-            sectionProducts, : .innerHTML += `
-        
-        `
-        });
+        insertCards(productsApi);
     });
 }
+function insertCards(productsApi) {
+    productsApi.forEach((product, index) => {
+        if (index <= 20) {
+            sectionProducts.innerHTML += `
+            <article class="cart">
+                <h2>${product.title}</h2>
+                <div id="img-product"><img src="${product.images[0].sp}" alt="Image Product"></div>
+                <div id="info-product">
+                    <p>${product.price}</p>
+                    <span>${product.description}</span>
+                    <p>${product.category.name}</p>
+                </div>
+            </article>
+        `;
+        }
+    });
+}
+llamadoApi();
 export {};
--- apiPlatzi/src/index.ts
+++ apiPlatzi/src/index.ts
@@ -2,15 +2,31 @@ import { IResponseProducts } from "../src/interface/i-response-products.js"
 
 const sectionProducts=document.querySelector(".section-products") as  HTMLElement;
 
-async function llamadoApi():Promise {
+async function llamadoApi():Promise<void> {
     const response= await fetch ("https://api.escuelajs.co/api/v1/products");
-    const productsApi=await response.json();
+    const productsApi:IResponseProducts[] =await response.json();
+    insertCards(productsApi);
+   
+}
 
-    productsApi.forEach(product:  => {
-        sectionProducts.innerHTML +=`
-        
+function insertCards(productsApi:IResponseProducts[]):void {
+    productsApi.forEach((product:IResponseProducts,index:number) => {
+
+        if(index<=20){
+            sectionProducts.innerHTML +=`
+            <article class="cart">
+                <h2>${product.title}</h2>
+                <div id="img-product"><img src="${product.images[0].sp}" alt="Image Product"></div>
+                <div id="info-product">
+                    <p>${product.price}</p>
+                    <span>${product.description}</span>
+                    <p>${product.category.name}</p>
+                </div>
+            </article>
         `
+        }
         
     });
 }
 
+llamadoApi()
