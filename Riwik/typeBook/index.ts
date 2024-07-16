7cecfe5edc679a7428ba95afe182335c48e767f0
Karina P <karina_pineda97@hotmail.com>
2024-07-16 08:01:22 -0500

    se realiza el login de un usuario con POST

--- Riwik/typeBook/index.ts
+++ Riwik/typeBook/index.ts
@@ -5,9 +5,42 @@ interface BodyRequestLogin{
     email:string,
     password:string
 }
-
-async function postLogin(data: BodyRequestLogin):Promise<void> {
-    const headers:Record<string, string> ={
-        ''
-    }
+interface BodyResponseLogin{
+    message:string,
+    data: Record<string, string>
 }
+
+async function postLogin(data: BodyRequestLogin):Promise<BodyResponseLogin> {
+    const headers:Record<string, string> ={
+        'Content-Type': 'application/json'
+        // 'Authorization': 'Bearer <token>'
+    }
+    const reqOptions: RequestInit={
+        method: 'POST',
+        headers:headers,
+        body: JSON.stringify(data)
+    }
+    const url: string= domain + endpointLogin;
+    const result:Response = await fetch(url, reqOptions);
+    
+    console.log(`Status code: ${result.status}`);
+    if(result.status !== 201) {
+        console.log(`Response body: ${(await result.json()).message}`);
+        throw new Error("Not authenticated: ");
+    }
+    const responseBodyLogin:BodyResponseLogin= await result.json();
+    console.log(`Result token: ${responseBodyLogin.data.token}`);
+    
+    return responseBodyLogin;
+}
+
+const dataToLogin:BodyRequestLogin={
+    email: 'prueba@prueba.pru',
+    password: 'C0ntr4S3gu++r4'
+}
+postLogin(dataToLogin).then((result:string):void=>{
+    console.log(result);
+}).catch((error):void=>{
+    console.log(` =( : ${error})`);
+
+})
